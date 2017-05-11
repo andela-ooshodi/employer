432a716b819b700d859b52546632f60985467dbf
Olufunmilade Oshodi <olufunmilade.oshodi@andela.com>
2017-05-11 11:37:42 +0100

    Setup Models

--- employer/api/models.py
+++ employer/api/models.py
@@ -2,4 +2,29 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Base(models.Model):
+    email = models.EmailField(max_length=50)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        """
+        String representation of model
+        :return:
+        """
+        return self.name
+
+
+class Employer(Base):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+
+
+class Employee(Base):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    dob = models.DateField()
+    verified = models.BooleanField()
+    employer = models.ForeignKey(Employer, related_name='employees')
